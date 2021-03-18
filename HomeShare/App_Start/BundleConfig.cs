8d0643dca942da460443f84158a4753e0aebdbb9
Omoik4ne <56385558+Omoik4ne@users.noreply.github.com>
2021-03-18 19:53:08 +0100

    layout et connection string

--- HomeShare/App_Start/BundleConfig.cs
+++ HomeShare/App_Start/BundleConfig.cs
@@ -16,15 +16,27 @@ namespace HomeShare
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.ba-cond.min.js",
+                        "~/Scripts/jquery.slitslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            "~/Scripts/owl.carousel.js",
+            "~/Scripts/modernizr.custom.79639.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/owl.carousel.css",
+                        "~/Content/owl.theme.css",
+                      "~/Content/custom.css",
+                      "~/Content/style.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/general/style.css"));
         }
+                //<script src = "http://code.jquery.com/jquery-1.9.1.min.js" ></ script >
+
     }
 }
